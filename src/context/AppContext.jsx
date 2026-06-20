2bf03263b232aafc20b2ae48f23b41337f01aed8
TradingMorning <falconspido.com@gmail.com>
2026-06-20 06:24:08 +0530

    Update import path for apiService in AppContext

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { apiService } from '../utils/api';
+import { apiService } from '@/./utils/api';
 
 const AppContext = createContext(undefined);
 
