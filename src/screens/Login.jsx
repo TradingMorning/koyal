c34313c2f885a7b2fa59f53e63f05ca6ecaaf871
TradingMorning <falconspido.com@gmail.com>
2026-06-20 06:08:41 +0530

    Update Login.jsx

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useLocation, Link } from 'react-router-dom';
